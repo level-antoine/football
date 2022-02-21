8f2e59f623ceb3bd22f4777bb1c0602c89b20b51
Level Antoine <level.toine@gmail.com>
2022-02-21 12:34:56 +0100

    FIRST IA

--- gfootball/play_game.py
+++ gfootball/play_game.py
@@ -22,16 +22,16 @@ from __future__ import print_function
 from absl import app
 from absl import flags
 from absl import logging
-
+import random
+import numpy as np
+import math
 
 from gfootball.env import config
 from gfootball.env import football_env
+from gfootball.env import football_action_set
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('players', 'keyboard:left_players=1',
-                    'Semicolon separated list of players, single keyboard '
-                    'player on the left by default')
 flags.DEFINE_string('level', '', 'Level to play')
 flags.DEFINE_enum('action_set', 'default', ['default', 'full'], 'Action set')
 flags.DEFINE_bool('real_time', True,
@@ -40,13 +40,11 @@ flags.DEFINE_bool('render', True, 'Whether to do game rendering.')
 
 
 def main(_):
-  players = FLAGS.players.split(';') if FLAGS.players else ''
-  assert not (any(['agent' in player for player in players])
-             ), ('Player type \'agent\' can not be used with play_game.')
+
   cfg_values = {
-      'action_set': FLAGS.action_set,
+      'action_set': 'simple',
+      'custom_display_stats': 'simple115',
       'dump_full_episodes': True,
-      'players': players,
       'real_time': FLAGS.real_time,
   }
   if FLAGS.level:
@@ -55,16 +53,56 @@ def main(_):
   env = football_env.FootballEnv(cfg)
   if FLAGS.render:
     env.render()
-  env.reset()
-  try:
-    while True:
-      _, _, done, _ = env.step([])
-      if done:
-        env.reset()
-  except KeyboardInterrupt:
-    logging.warning('Game stopped, writing dump...')
-    env.write_dump('shutdown')
-    exit(1)
+  obs = env.reset()
+
+  while True:
+    active_player = player_nearst_to_ball(obs)
+    if check_if_adverse_player_is_nearst(active_player, obs):
+      for player in obs[0]['left_team']:
+        print(player)
+        print(active_player)
+        if player[0] != active_player[0] and player[1] != active_player[1]:
+          if distance(player, active_player) < 0.2:
+            obs, reward, done, info = env.step(football_action_set.action_short_pass)
+          elif distance(player, active_player) < 0.5:
+            obs, reward, done, info = env.step(football_action_set.action_long_pass)
+        else:
+          obs, reward, done, info = env.step(football_action_set.action_dribble)
+    elif -0.04 < active_player[1] < 0.04:
+      if np.sqrt(pow(obs[0]['ball'][1] - (-1), 2) + pow(obs[0]['ball'][1] - 0.00, 2)) < 0.3:
+        obs, reward, done, info = env.step(football_action_set.action_shot)
+      else:
+        obs, reward, done, info = env.step(football_action_set.action_right)
+    else:
+      if active_player[1] > 0.00:
+        obs, reward, done, info = env.step(football_action_set.action_top)
+      else:
+        obs, reward, done, info = env.step(football_action_set.action_bottom)
+    if done:
+      env.reset()
+
+
+def player_nearst_to_ball(obs):
+  min_dist = np.inf
+  player_coo = ()
+  for player in obs[0]['left_team']:
+    # le plus proche du ballon
+    d = distance(player, obs[0]['ball'])
+    if min_dist > d:
+      min_dist = d
+      player_coo = (player[0], player[1])
+  return player_coo
+
+
+def check_if_adverse_player_is_nearst(player_pos, obs):
+  for adverse_player in obs[0]['right_team']:
+    if distance(player_pos, adverse_player) < 0.1:
+      return True
+  return False
+
+
+def distance(first_obj, second_obj):
+  return np.sqrt(pow(first_obj[0] - second_obj[0], 2) + pow(first_obj[1] - second_obj[1], 2))
 
 
 if __name__ == '__main__':
